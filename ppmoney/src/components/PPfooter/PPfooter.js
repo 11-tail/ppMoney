477cd51dc5ed9636d393b51aa3a671f99a16ae17
masturiri <2720601448@qq.com>
2019-04-29 10:09:53 +0800

    尝试合并

---
+++ ppmoney/src/components/PPfooter/PPfooter.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './PPfooter.less'
+
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            // title: "头部"
+        }
+    }
+    render() {
+        return (
+            <div>
+                <div className="g-ft-wrap">
+                    <div className="g-ft">
+                    
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Footer
--- ppmoney/src/routes/HomePage/HomePage.js
+++ ppmoney/src/routes/HomePage/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'dva';
 import PPhead from '../../components/PPheade/PPhead';
+import PPfooter from '../../components/PPfooter/PPfooter';
 function HomePage() {
     return (
         <div>
             <PPhead />
-            
+            <PPfooter />
         </div>
     );
 }
